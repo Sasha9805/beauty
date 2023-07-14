ff90bc29d322768e7b4a57b74fe799bff2934d35
Sasha <pimenovs33@gmail.com>
2023-07-14 19:55:21 +0300

    Added useHttp hook

--- src/components/appointmentItem/AppointmentItem.tsx
+++ src/components/appointmentItem/AppointmentItem.tsx
@@ -4,10 +4,14 @@ function AppointmentItem() {
 	return (
 		<div className="appointment">
 			<div className="appointment__info">
-				<span className="appointment__date">Date: DD/MM/YYYY HH:mm</span>
+				<span className="appointment__date">
+					Date: DD/MM/YYYY HH:mm
+				</span>
 				<span className="appointment__name">Name: Alex Smith</span>
 				<span className="appointment__service">Service: Manicure</span>
-				<span className="appointment__phone">Phone: +1 948 945 344</span>
+				<span className="appointment__phone">
+					Phone: +1 948 945 344
+				</span>
 			</div>
 			<div className="appointment__time">
 				<span>Time left:</span>
