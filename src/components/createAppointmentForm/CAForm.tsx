aab98343a49c701f26e63ac3d2d10a94494a8eff
Sasha <pimenovs33@gmail.com>
2023-07-30 21:36:58 +0300

    Added react-hook-form

--- src/components/createAppointmentForm/CAForm.tsx
+++ src/components/createAppointmentForm/CAForm.tsx
@@ -1,116 +1,124 @@
-import { useState, useContext } from "react";
+import { useContext } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import useAppointmentService from "../../services/AppointmentService";
 import { AppointmentContext } from "../../context/appointments/AppointmentsContext";
 
 import { IAppointment } from "../../shared/interfaces/appointment.interface";
 import "./caform.scss";
 
+interface ICAForm {
+	name: string;
+	service: string;
+	phone: string;
+	date: string;
+}
+
+const schema = yup.object({
+	name: yup.string().required("Name is required"),
+	service: yup.string().required("Service is required"),
+	phone: yup
+		.string()
+		.required("Phone is required")
+		.matches(
+			/^\++[0-9]{1} [0-9]{3} [0-9]{3} [0-9]{3}$/,
+			"Format should be +1 804 944 567"
+		),
+	date: yup
+		.string()
+		.required("Date is required")
+		.matches(
+			/^\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}$/,
+			"Format should be DD/MM/YYYY HH:mm"
+		),
+});
+
 function CAForm() {
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors, isSubmitting },
+	} = useForm<ICAForm>({
+		mode: "onBlur",
+		defaultValues: {
+			date: "",
+			name: "",
+			service: "",
+			phone: "",
+		},
+		resolver: yupResolver(schema),
+	});
+
 	const { createNewAppointment } = useAppointmentService();
 	const { getActiveAppointments } = useContext(AppointmentContext);
 
-	const [formData, setFormData] = useState<IAppointment>({
-		id: 1,
-		date: "",
-		name: "",
-		service: "",
-		phone: "",
-		canceled: false,
-	});
-	const [creationStatus, setCreationStatus] = useState(false);
-
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		setCreationStatus(true);
-		
-		createNewAppointment(formData)
-			.then(() => {
-				setCreationStatus(false);
-				setFormData({
-					id: 1,
-					date: "",
-					name: "",
-					service: "",
-					phone: "",
-					canceled: false,
-				});
-				getActiveAppointments();
-			})
-			.catch(() => {
-				setCreationStatus(false);
-				alert("Error while creating new appointment");
-			});
-	};
-
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = e.target;
-		setFormData((prevData) => ({
-			...prevData,
-			[name]: value,
-		}));
+	const onSubmit: SubmitHandler<ICAForm> = async (data) => {
+		const appointmentData: IAppointment = {
+			id: 1,
+			...data,
+			canceled: false,
+		};
+		try {
+			await createNewAppointment(appointmentData);
+			reset();
+			getActiveAppointments();
+		} catch (e) {
+			alert("Error while creating new appointment");
+		}
 	};
 
 	return (
-		<form className="caform" onSubmit={handleSubmit}>
+		<form className="caform" onSubmit={handleSubmit(onSubmit)}>
 			<div className="caform__title">Create new appointment</div>
 			<label htmlFor="name">
 				Name<span>*</span>
 			</label>
 			<input
+				{...register("name")}
 				type="text"
-				name="name"
 				id="name"
 				placeholder="User name"
-				required
-				value={formData.name}
-				onChange={handleChange}
 			/>
+			<div className="caform__error">{errors.name?.message}</div>
 
 			<label htmlFor="service">
 				Service<span>*</span>
 			</label>
 			<input
+				{...register("service")}
 				type="text"
-				name="service"
 				id="service"
 				placeholder="Service name"
-				required
-				value={formData.service}
-				onChange={handleChange}
 			/>
+			<div className="caform__error">{errors.service?.message}</div>
 
 			<label htmlFor="phone">
 				Phone number<span>*</span>
 			</label>
 			<input
+				{...register("phone")}
 				type="tel"
-				name="phone"
 				id="phone"
 				placeholder="+1 890 335 372"
-				pattern="^\++[0-9]{1} [0-9]{3} [0-9]{3} [0-9]{3}"
 				title="Format should be +1 804 944 567"
-				required
-				value={formData.phone}
-				onChange={handleChange}
 			/>
+			<div className="caform__error">{errors.phone?.message}</div>
 
 			<label htmlFor="date">
 				Date<span>*</span>
 			</label>
 			<input
+				{...register("date")}
 				type="text"
-				name="date"
 				id="date"
 				placeholder="DD/MM/YYYY HH:mm"
-				pattern="^\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}$"
 				title="Format should be DD/MM/YYYY HH:mm"
-				required
-				value={formData.date}
-				onChange={handleChange}
 			/>
-			<button type="submit" disabled={creationStatus}>
-				Create
-			</button>
+			<div className="caform__error">{errors.date?.message}</div>
+
+			<button disabled={isSubmitting}>Create</button>
 		</form>
 	);
 }
